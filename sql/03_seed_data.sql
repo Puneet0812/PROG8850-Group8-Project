f43b38bbb41b45ed82a1cfac6361ec1f72b105d5
Puneet Mishra <pmishra0233@conestogac.on.ca>
2025-04-16 14:38:00 -0400

    Initial CI/CD pipeline setup

---
+++ sql/03_seed_data.sql
@@ -0,0 +1,6 @@
+USE project_db;
+INSERT INTO ClimateData (location, record_date, temperature, precipitation, humidity)
+VALUES 
+('Delhi', '2024-05-01', 35.0, 0.0, 30.0),
+('Mumbai', '2024-05-01', 30.5, 12.0, 70.0),
+('Chennai', '2024-05-01', 34.2, 5.0, 80.0);
